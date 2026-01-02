2442301bba05c14be5cd5b958d952527129f15ec
Kirn Gill II <segin2005@gmail.com>
2026-01-02 04:38:36 -0600

    Fix TSS limit calculation and restore user segment loading
    Critical bug: TSS limit was base+size instead of just size-1
    This made the TSS descriptor invalid, breaking ring transitions
    
    Also restored DS/ES/FS/GS = 0x23 before IRET (it IS allowed)

--- sys/arch/i386/gdt.c
+++ sys/arch/i386/gdt.c
@@ -21,7 +21,7 @@ static void gdt_set_gate(int32_t num, uint32_t base, uint32_t limit, uint8_t acc
 
 static void write_tss(int32_t num, uint16_t ss0, uint32_t esp0) {
     uint32_t base = (uint32_t) &tss_entry;
-    uint32_t limit = base + sizeof(tss_entry);
+    uint32_t limit = sizeof(tss_entry) - 1;  // Limit is the SIZE, not base + size!
 
     gdt_set_gate(num, base, limit, 0xE9, 0x00);
 
