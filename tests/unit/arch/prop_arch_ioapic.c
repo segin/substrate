3373bcc4e42a5725a77e017b85d42682ba06eb82
Kirn Gill II <segin2005@gmail.com>
2026-01-01 19:03:33 -0600

    test(arch): Add property-based tests for IO-APIC

---
+++ tests/unit/arch/prop_arch_ioapic.c
@@ -0,0 +1,21 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include "../../../sys/arch/i386/ioapic.h"
+
+/*
+ * Property-based test: IO-APIC Invariant
+ * Prop: ioapic_set_mask(irq, true) -> REDTBL[irq] & (1<<16) != 0.
+ */
+
+bool prop_ioapic_mask_invariant(uint32_t redtbl_val, bool masked) {
+    if (masked) {
+        return (redtbl_val & (1 << 16));
+    } else {
+        return !(redtbl_val & (1 << 16));
+    }
+}
+
+void run_ioapic_properties(void) {
+    prop_ioapic_mask_invariant(1 << 16, true);
+    prop_ioapic_mask_invariant(0, false);
+}
