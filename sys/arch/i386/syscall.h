65df622163ac208c287677aa3ec56e4f03136c04
Kirn Gill II <segin2005@gmail.com>
2026-01-20 03:04:28 -0600

    Refactor native syscalls to use symbolic constants
    sys/arch/i386/syscall.h:
    - Added missing SYS_* definitions for all implemented native syscalls (e.g., LSEEK, GETPID, MOUNT, STAT, etc.).
    - Ensures consistency between syscall definition and usage.
    
    sys/exec/perso/perso_native.c:
    - Replaced hardcoded syscall numbers with SYS_* symbolic constants in native_syscalls, native_names, and native_fmts tables.
    - aligned formatting.

--- sys/arch/i386/syscall.h
+++ sys/arch/i386/syscall.h
@@ -8,6 +8,7 @@
 #define MAX_SYSCALLS 512
 
 // Syscall Numbers
+// Syscall Numbers
 #define SYS_EXIT    1
 #define SYS_FORK    2
 #define SYS_READ    3
@@ -21,22 +22,52 @@
 #define SYS_EXECVE  11
 #define SYS_CHDIR   12
 #define SYS_TIME    13
-#define SYS_CHROOT  61
 #define SYS_MKNOD   14
 #define SYS_CHMOD   15
 #define SYS_LCHOWN  16
-#define SYS_IOCTL   54
+#define SYS_LSEEK   19
+#define SYS_GETPID  20
+#define SYS_MOUNT   21
+#define SYS_UMOUNT  22
+#define SYS_SETUID  23
+#define SYS_GETUID  24
+#define SYS_ACCESS  33
+#define SYS_SYNC    36
+#define SYS_KILL    37
+#define SYS_MKDIR   39
+#define SYS_RMDIR   40
 #define SYS_DUP     41
+#define SYS_PIPE    42
+#define SYS_BRK     45
+#define SYS_SETGID  46
+#define SYS_GETGID  47
+#define SYS_SIGNAL  48
+#define SYS_GETEUID 49
+#define SYS_GETEGID 50
+#define SYS_ACCT    51
+#define SYS_IOCTL   54
+#define SYS_CHROOT  61
+#define SYS_DUP2    63
+#define SYS_READLINK 85
+#define SYS_MMAP    90
+#define SYS_MUNMAP  91
+#define SYS_TRUNCATE 92
+#define SYS_FTRUNCATE 93
+#define SYS_STAT    106
 #define SYS_LSTAT   107
 #define SYS_FSTAT   108
-#define SYS_CLONE   120
-#define SYS_FUTEX   240
-#define SYS_MSYNC   144
-#define SYS_GET_COW_STATS 241
-#define SYS_POLL    209
 #define SYS_SIGRETURN 119
+#define SYS_CLONE   120
+#define SYS_UNAME   122
+#define SYS_GETDENTS 141
+#define SYS_MSYNC   144
+#define SYS_NANOSLEEP 162
+#define SYS_GETCWD  183
 #define SYS_SIGALTSTACK 186
-// ... add more as needed
+#define SYS_POLL    209
+#define SYS_FUTEX   240
+#define SYS_PMAP_STATS 241
+#define SYS_THR_NEW 455
 
 void syscall_init(void);
 
