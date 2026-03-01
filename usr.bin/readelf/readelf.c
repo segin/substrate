fca1ff20ed7c3dc3189659683992864ec574fbfa
Kirn Gill II <segin2005@gmail.com>
2026-03-01 08:33:12 -0600

    readelf: implement section 6b x86-64 relocation decoding

--- usr.bin/readelf/readelf.c
+++ usr.bin/readelf/readelf.c
@@ -924,9 +924,50 @@ static const char *reloc_type_i386(uint32_t type) {
     }
 }
 
+static const char *reloc_type_x86_64(uint32_t type) {
+    switch (type) {
+        case 0: return "R_X86_64_NONE";
+        case 1: return "R_X86_64_64";
+        case 2: return "R_X86_64_PC32";
+        case 3: return "R_X86_64_GOT32";
+        case 4: return "R_X86_64_PLT32";
+        case 5: return "R_X86_64_COPY";
+        case 6: return "R_X86_64_GLOB_DAT";
+        case 7: return "R_X86_64_JUMP_SLOT";
+        case 8: return "R_X86_64_RELATIVE";
+        case 9: return "R_X86_64_GOTPCREL";
+        case 10: return "R_X86_64_32";
+        case 11: return "R_X86_64_32S";
+        case 12: return "R_X86_64_16";
+        case 13: return "R_X86_64_PC16";
+        case 14: return "R_X86_64_8";
+        case 15: return "R_X86_64_PC8";
+        case 16: return "R_X86_64_DTPMOD64";
+        case 17: return "R_X86_64_DTPOFF64";
+        case 18: return "R_X86_64_TPOFF64";
+        case 19: return "R_X86_64_TLSGD";
+        case 20: return "R_X86_64_TLSLD";
+        case 21: return "R_X86_64_DTPOFF32";
+        case 22: return "R_X86_64_GOTTPOFF";
+        case 23: return "R_X86_64_TPOFF32";
+        case 24: return "R_X86_64_PC64";
+        case 25: return "R_X86_64_GOTOFF64";
+        case 26: return "R_X86_64_GOTPC32";
+        case 32: return "R_X86_64_SIZE32";
+        case 33: return "R_X86_64_SIZE64";
+        case 37: return "R_X86_64_IRELATIVE";
+        case 41: return "R_X86_64_GOTPCRELX";
+        case 42: return "R_X86_64_REX_GOTPCRELX";
+        default: return NULL;
+    }
+}
+
 static const char *reloc_type_name(uint16_t machine, uint32_t type) {
     if (machine == EM_386) {
         return reloc_type_i386(type);
+    }
+    if (machine == EM_X86_64) {
+        return reloc_type_x86_64(type);
     }
     return NULL;
 }
