cde51b3799bec8c2cf87e2a47e8685a39c496581
Kirn Gill II <segin2005@gmail.com>
2026-03-05 04:12:42 -0600

    Remove stale test artifacts from repo root

--- test_Makefile_modify.sh
+++
@@ -1,1 +0,0 @@
-sed -i 's/host_test_crc32/host_test_crc32 host_test_compat_lseek32/g' tests/sys/Makefile
--- test_bios_13.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Test script to check INT 10h AH=13h behavior.\n");
-    return 0;
-}
